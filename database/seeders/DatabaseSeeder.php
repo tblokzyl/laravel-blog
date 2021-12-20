2fed330bf7ce9953d5d26e9ed2d98ec4aac91c32
tblokzyl <tblokzyl@gmail.com>
2021-12-19 22:55:37 -0500

    Added Authors Page

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,9 +20,14 @@ class DatabaseSeeder extends Seeder
         Post::truncate();
         Category::truncate();
 
-        $user = User::factory()->create();
+        $user = User::factory()->create([
+            'name' => 'John Doe',
+        ]);
+        Post::factory(5)->create([
+            'user_id' => $user->id
+        ]);
 
-        $family = Category::create([
+/*         $family = Category::create([
             'name' => 'Family',
             'slug' => 'family'
         ]);
@@ -55,6 +60,6 @@ class DatabaseSeeder extends Seeder
             'excerpt' => '<p>Lorem ipsum dolar sit amet</p>',
             'body' => '<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>',
 
-        ]);
+        ]); */
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('posts', [
-        'posts' => Post::with('category')->get()
+        'posts' => Post::latest()->get()
     ]);
 });
 
@@ -29,5 +29,11 @@ Route::get('/posts/{post}', function ($id) {
 Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [
         'posts' => $category->posts
+    ]);
+});
+
+Route::get('/authors/{author:username}', function (User $author) {
+    return view('posts', [
+        'posts' => $author->posts 
     ]);
 });
