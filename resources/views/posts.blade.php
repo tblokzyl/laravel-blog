02f271e834e10203d0d6b81c13e50f83a7233662
tblokzyl <tblokzyl@gmail.com>
2021-12-21 23:16:44 -0500

    Adding frontend and hooking it up to the backend

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,17 +1,10 @@
 <x-layout>
-    @foreach ($posts as $post)
-        <article>
-            <h1>
-                <a href="/posts/{{ $post->id }}">
-                    {{ $post->title }}
-                </a>
-            </h1>
-            <p>
-                By <a href="/authors/{{ $post->author->username }}"> {{ $post->author->name }} </a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
-            </p>
-            <div>
-                {!! $post->excerpt !!}
-            </div>
-        </article>
-    @endforeach
+    @include('_posts-header')
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        @if ( $posts->count())
+            <x-post-grid :posts="$posts"/>
+        @else
+            <p class="text-center">No posts yet check back later.</p>
+        @endif
+    </main>
 </x-layout>
