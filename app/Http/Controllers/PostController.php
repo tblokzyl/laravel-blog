5c4d6d75205b287d0975b102c5184599d5b3b43d
tblokzyl <tblokzyl@gmail.com>
2021-12-26 20:56:21 -0500

    category & author filtering + pagination

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,15 +11,16 @@ class PostController extends Controller
     public function index()
     {
         
-        return view('posts', [
-            'posts' => Post::latest()->filter(request(['search']))->get(),
-            'categories' => Category::all()
+        return view('posts.index', [
+            'posts' => Post::latest()->filter(
+                request(['search', 'category', 'author'])
+            )->paginate(6)->withQueryString()
         ]);
     }
 
     public function show(Post $post)
     {
-        return view('post', [
+        return view('posts.show', [
             'post' => $post
         ]);
     }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -14,11 +14,22 @@ class Post extends Model
     protected $with = ['category', 'author'];
 
     public function scopeFilter($query, array $filters) {
-        if ($filters['search'] ?? false) {
-            $query
-                ->where('title', 'like', '%' . request('search') . '%')
-                ->orWhere('body', 'like', '%' . request('search') . '%');
-        }
+        $query->when( $filters['search'] ?? false, fn($query, $search) => 
+            $query->where(fn($query) => 
+                $query->where('title', 'like', '%' . request('search') . '%')
+                ->orWhere('body', 'like', '%' . request('search') . '%')
+            )
+        );
+
+        $query->when($filters['category'] ?? false, fn($query, $category) =>
+        $query->whereHas('category', fn($query) => 
+            $query->where('slug', $category))
+        );
+
+        $query->when($filters['author'] ?? false, fn($query, $author) =>
+        $query->whereHas('author', fn($query) => 
+            $query->where('username', $author))
+        );
     }
 
     public function category() {
