02f72cb79d911c1fe855d0b285c7319055830f11
tblokzyl <tblokzyl@gmail.com>
2021-12-30 18:21:56 -0500

    Setup newsletter with mailchimp and used an interface to accept multiple email marketing platforms

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,12 @@
 
 namespace App\Providers;
 
+use App\Services\Newsletter;
+use App\Services\MailchimpNewsletter;
 use Illuminate\Contracts\Pagination\Paginator;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
+use MailchimpMarketing\ApiClient;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,7 +18,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        app()->bind(Newsletter::class, function() {
+            $client = (new ApiClient())->setConfig([
+                'apiKey' => config('services.mailchimp.key'),
+                'server' => 'us20'
+            ]);
+
+            return new MailchimpNewsletter($client);
+        });
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -5,17 +5,21 @@ namespace App\Models;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\PostCommentsController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\NewsletterController;
 use App\Http\Controllers\SessionsController;
+use App\Services\Newsletter;
+use Illuminate\Validation\ValidationException;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('/posts/{post:slug}', [PostController::class, 'show']);
+Route::post('/posts/{post:slug}/comments', [PostCommentsController::class, 'store']);
+
+Route::post('newsletter', NewsletterController::class);
 
 Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
 Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
-
-Route::post('/posts/{post:slug}/comments', [PostCommentsController::class, 'store']);
 
 Route::get('login', [SessionsController::class, 'create'])->middleware('guest');
 Route::post('login', [SessionsController::class, 'store'])->middleware('guest');
